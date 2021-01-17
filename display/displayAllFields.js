43d062d0fd271bfcd76c9c389f386e74cabff7bf
TakashiAihara <takashi.aihara.0523@gmail.com>
2021-01-17 17:46:13 +0900

    fix wait

--- display/displayAllFields.js
+++ display/displayAllFields.js
@@ -1,2 +1,11 @@
-Object.keys(kintone.app.record.get().record).map(e => kintone.app.record.setFieldShown(e, true))
-Object.entries(cybozu.data.page.FORM_DATA.schema.table.fieldList).map(e => kintone.app.record.setFieldShown(e[1].var, true))
+(function() {
+    'use strict';
+    $(window).on('load', async function(){
+        console.log("test");
+        await sleep(1000);
+        Object.keys(kintone.app.record.get().record).map(e => kintone.app.record.setFieldShown(e, true));
+        Object.entries(cybozu.data.page.FORM_DATA.schema.table.fieldList).map(e => kintone.app.record.setFieldShown(e[1].var, true));
+        });
+})();
+
+const sleep = msec => new Promise(resolve => setTimeout(resolve, msec));
