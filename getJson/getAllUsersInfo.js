9c0cca28e354e2454b4e4be87d2fe258923dd2e2
TakashiAihara <takashi.aihara.0523@gmail.com>
2021-04-04 20:09:38 +0900

    getAllUsersInfo.js

--- getJson/getAllUsersInfo.js
+++ getJson/getAllUsersInfo.js
@@ -1,3 +1,10 @@
+/*
+  全ユーザーの所属組織とグループを出力する。
+  各環境のkintoneコマンドが利用可能な画面で実行する。
+*/
+
+const promiseObject = {};
+
 async function getUsers({ record = [], offset = 0 } = {}) {
   var userInfo = await kintone.api(kintone.api.url('/v1/users', true), 'GET', { offset: offset });
   if (userInfo.users.length === 100) {
@@ -6,3 +13,36 @@ async function getUsers({ record = [], offset = 0 } = {}) {
     return userInfo.users;
   }
 }
+
+async function getOrganizationsTitles({ code } = {}) {
+  const api = '/v1/user/organizations';
+  if (!promiseObject[`${api}${code}`]) {
+    promiseObject[`${api}${code}`] = kintone.api(kintone.api.url(api, true), 'GET', { code: code });
+  }
+  var userInfo = await promiseObject[`${api}${code}`];
+  return userInfo.organizationTitles;
+}
+
+async function getGroups({ code } = {}) {
+  const api = '/v1/user/groups';
+  if (!promiseObject[`${api}${code}`]) {
+    promiseObject[`${api}${code}`] = kintone.api(kintone.api.url(api, true), 'GET', { code: code });
+  }
+  var userInfo = await promiseObject[`${api}${code}`];
+  return userInfo.groups;
+}
+
+copy(await Promise.all((await getUsers())
+  .map(async (user) => {
+    var organizationTitles = await getOrganizationsTitles({ code: user.code });
+    var groups = await getGroups({ code: user.code });
+    return {
+      user: { code: user.code, name: user.name },
+      organizations: organizationTitles.map(organization => { return { code: organization.code, name: organization.name } }),
+      groups: groups.map(group => { return { code: group.code, name: group.name } }),
+    };
+  }
+  )
+)
+
+)
