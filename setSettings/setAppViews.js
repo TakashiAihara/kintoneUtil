159460fd2b20762dad4404382587d2ba4b9e62dd
TakashiAihara <takashi.aihara.0523@gmail.com>
2021-04-04 20:48:38 +0900

    update setAppViews.js

--- setSettings/setAppViews.js
+++ setSettings/setAppViews.js
@@ -1,17 +1,36 @@
-// viwsにはviews:{}以下のものを追記する
-// 例：一覧: {},自分が作業者:{}
-var views = {};
+// view 変更を適用する
+// アプリのレコード画面で実行する。
+
+var views =
+//  ================= ここに JSON 展開 =================
+{
+  views: {
+    一覧: {
+      type: 'LIST',
+      name: '一覧',
+      filterCond: '',
+      sort: '',
+      index: '1',
+      fields: [
+        'レコード番号'
+      ]
+    }
+  }
+};
 
 await kintone.api(kintone.api.url('/k/v1/preview/app/views', true), 'PUT', {
   app: kintone.app.getId(),
-  views,
+  views: views.views
 });
+console.log("applied to preview.")
 
 // deploy
 await kintone.api(kintone.api.url('/k/v1/preview/app/deploy', true), 'POST', {
   apps: [
     {
-      app: kintone.app.getId(),
-    },
-  ],
+      app: kintone.app.getId()
+    }
+  ]
 });
+
+console.log("deployed.")
