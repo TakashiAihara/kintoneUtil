29fcd8969c317ef05e3259f7eac34933690ae12e
TakashiAihara <takashi.aihara.0523@gmail.com>
2021-04-04 19:39:59 +0900

    modify getAllSettings.js

--- getJson/getAllSettings.js
+++ getJson/getAllSettings.js
@@ -1,13 +1,17 @@
-// 全設定取得JS
-// 各環境のアプリ管理画面で実行すること。
-// エラーになる場合は、アプリ名が異なる可能性がある。
+// アプリ管理画面での複数アプリ全設定取得JS
+// エラーになる場合は、アプリIDが異なるか、権限が無い可能性がある。
 
-const appTitleList = [];
+// アプリID（複数）
+const appTitleList = [
+  1,
+  2,
+  3
+];
 
-const appObj = appTitleList.map((e) => {
+const appObj = appTitleList.map((id) => {
   return {
-    id: $x(`//*[td/*[text()="${e}"]]`)[0].firstElementChild.textContent,
-    name: e,
+    id: id,
+    name: $x(`//*[td[text()="${id}"]]/td[2]/a/text()`)[0].textContent
   };
 });
 
@@ -21,47 +25,43 @@ function downloadJson(fileName, json) {
 
 for (v of appObj) {
   downloadJson(
-    `${v.name}_${location.hostname.split('.')[0]}_フィールドの一覧.json`,
+    `${v.name}_${location.hostname.split('.')[0]}_app_form_fields.json`,
     await kintone.api(kintone.api.url('/k/v1/app/form/fields', true), 'GET', { app: v.id })
   );
   downloadJson(
-    `${v.name}_${location.hostname.split('.')[0]}_フォームのレイアウト.json`,
+    `${v.name}_${location.hostname.split('.')[0]}_app_form_layout.json`,
     await kintone.api(kintone.api.url('/k/v1/app/form/layout', true), 'GET', { app: v.id })
   );
   downloadJson(
-    `${v.name}_${location.hostname.split('.')[0]}_一覧の設定.json`,
+    `${v.name}_${location.hostname.split('.')[0]}_app_views.json`,
     await kintone.api(kintone.api.url('/k/v1/app/views', true), 'GET', { app: v.id })
   );
   downloadJson(
-    `${v.name}_${location.hostname.split('.')[0]}_アプリ情報.json`,
-    await kintone.api(kintone.api.url('/k/v1/app', true), 'GET', { id: v.id })
-  );
-  downloadJson(
-    `${v.name}_${location.hostname.split('.')[0]}_一般設定.json`,
+    `${v.name}_${location.hostname.split('.')[0]}_app_settings.json`,
     await kintone.api(kintone.api.url('/k/v1/app/settings', true), 'GET', { app: v.id })
   );
   downloadJson(
-    `${v.name}_${location.hostname.split('.')[0]}_プロセス管理の設定.json`,
+    `${v.name}_${location.hostname.split('.')[0]}_app_status.json`,
     await kintone.api(kintone.api.url('/k/v1/app/status', true), 'GET', { app: v.id })
   );
   downloadJson(
-    `${v.name}_${location.hostname.split('.')[0]}_JavaScript / CSS.json`,
+    `${v.name}_${location.hostname.split('.')[0]}_app_customize.json`,
     await kintone.api(kintone.api.url('/k/v1/app/customize', true), 'GET', { app: v.id })
   );
   downloadJson(
-    `${v.name}_${location.hostname.split('.')[0]}_アプリのアクセス権.json`,
+    `${v.name}_${location.hostname.split('.')[0]}_app_acl.json`,
     await kintone.api(kintone.api.url('/k/v1/app/acl', true), 'GET', { app: v.id })
   );
   downloadJson(
-    `${v.name}_${location.hostname.split('.')[0]}_レコードのアクセス権.json`,
+    `${v.name}_${location.hostname.split('.')[0]}_record_acl.json`,
     await kintone.api(kintone.api.url('/k/v1/record/acl', true), 'GET', { app: v.id })
   );
   downloadJson(
-    `${v.name}_${location.hostname.split('.')[0]}_フィールドのアクセス権.json`,
+    `${v.name}_${location.hostname.split('.')[0]}_field_acl.json`,
     await kintone.api(kintone.api.url('/k/v1/field/acl', true), 'GET', { app: v.id })
   );
   downloadJson(
-    `${v.name}_${location.hostname.split('.')[0]}_フォーム設計情報.json`,
+    `${v.name}_${location.hostname.split('.')[0]}_form.json`,
     await kintone.api(kintone.api.url('/k/v1/form', true), 'GET', { app: v.id })
   );
 }
